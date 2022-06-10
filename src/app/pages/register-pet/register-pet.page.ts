940f6c49a522104739247a46d24eb9542925dc05
Yerko Tolosa Norambuena <42778767+YerkoTolosa@users.noreply.github.com>
2022-06-10 17:05:24 -0400

    Crear reporte, Listo!

--- src/app/pages/register-pet/register-pet.page.ts
+++ src/app/pages/register-pet/register-pet.page.ts
@@ -10,6 +10,7 @@ import { reporte } from 'src/app/models/models';
 import { Geolocation, GeolocationOptions } from '@awesome-cordova-plugins/geolocation/ngx';
 
 
+
 @Component({
   selector: 'app-register-pet',
   templateUrl: './register-pet.page.html',
@@ -30,6 +31,7 @@ export class RegisterPetPage implements OnInit {
     lon: 0,
   }
 
+  randomId = "";
   uid = '';
   lat = 0;
   lon = 0;
@@ -37,6 +39,8 @@ export class RegisterPetPage implements OnInit {
   accur: number;
   options : GeolocationOptions;
   pet = null;
+  newFile: any;
+  getPetImage = null;
 
   constructor(
     private avatarService: AvatarService,
@@ -45,7 +49,8 @@ export class RegisterPetPage implements OnInit {
     private alertController: AlertController,
     private firestore: FirestoreService,
     private interaction: InteractionService,
-    private geolocation: Geolocation
+    private geolocation: Geolocation,
+    private router: Router
     
   ) 
   {
@@ -61,11 +66,9 @@ export class RegisterPetPage implements OnInit {
       }).catch((error) => {
         console.log('HOGAR-TEMPORAL: Error al obtener tu ubicacion', error);
       });
-
-
-    this.avatarService.getPet().subscribe((data) => {
+    /* this.avatarService.getPet().subscribe((data) => {
       this.pet = data;
-    });
+    }); */
    }
 
 
@@ -76,7 +79,7 @@ export class RegisterPetPage implements OnInit {
   }
 
 
-  async changePhoto() {
+  /* async changePhoto() {
     const image = await Camera.getPhoto({
       quality: 90,
       allowEditing: false,
@@ -105,20 +108,81 @@ export class RegisterPetPage implements OnInit {
       }
       console.log("HOGAR-TEMPORAL: IMAGEN CARGADA CORRECTAMENTE");
     }
+  } */
+
+  async changePhoto() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Base64,
+      source: CameraSource.Camera, // Camera, Photos or Prompt!
+    });
+
+    if (image) {
+      const loading = await this.loadingController.create({
+          message: 'Cargando Imagen',
+          spinner: "bubbles"
+      });
+      await loading.present();
+      const path = 'reportes';
+      const generarToken = this.firestore.createRandomID();
+      this.randomId = generarToken
+      /* const result = await this.avatarService.uploadImageReporte(image, path, name); */
+      const result = await this.avatarService.uploadPhoto(image, path, this.randomId); 
+      loading.dismiss();
+      this.data.url = result;
+
+      if (!result) {
+        const alert = await this.alertController.create({
+          header: 'Subida fallida',
+          message: 'Hubo un problema al cargar tu foto',
+          buttons: ['OK'],
+        });
+        console.log("HOGAR-TEMPORAL: ERROR AL CARGAR IMAGEN");
+        await alert.present();
+      }
+      console.log("HOGAR-TEMPORAL: IMAGEN CARGADA CORRECTAMENTE");
+
+      this.avatarService.getPetImage(this.randomId).subscribe((data) => {
+        this.getPetImage = data;
+      });
+     /*  this.avatarService.getPetImage(this.randomId); */
+    }
   }
+
+
+/*   async newImageUpload(event: any) {
+    if (event.target.files && event.target.files[0]) {
+        this.newFile = event.target.files[0];
+        const reader = new FileReader();
+        reader.onload = ((image) => {
+          
+        });
+        reader.readAsDataURL(event.target.files[0]);
+      }
+      const path = 'reportes';
+      const name = this.firestore.createRandomID();
+      if (this.newFile !== undefined) {
+        const res = await this.avatarService.uploadImageReporte(this.newFile, path, name);
+        this.data.url = res;
+      }
+   } */
+
 
 
    registrarPet(){
     const path = 'reportes';
-    const randomid = this.firestore.createRandomID();
+    const token = this.randomId
+    /* const randomid = this.firestore.createRandomID(); */
     const usuario = this.authService.test() 
     this.data.uid = usuario;
     this.data.lat = this.lat;
     this.data.lon = this.lon
-    this.firestore.createDocument(this.data, path, randomid).then( (res) => {
-      console.log('HOGAR-TEMPORAL: ID ASIGNADO A ESTE REPORTE: ', randomid);
+    this.firestore.createDocument(this.data, path, token).then( (res) => {
+      console.log('HOGAR-TEMPORAL: ID ASIGNADO A ESTE REPORTE: ', token);
       console.log('HOGAR-TEMPORAL: SE REGISTRO UNA MASCOTA EXITOSAMENTE: ', res);
     });
+    this.router.navigateByUrl('home', { replaceUrl: true });
   }
 
   
--- src/app/services/avatar.service.ts
+++ src/app/services/avatar.service.ts
@@ -6,6 +6,7 @@ import { Photo } from '@capacitor/camera';
 import { AuthService } from './auth.service';
 import { finalize } from 'rxjs/operators';
 import { FirestoreService } from './firestore.service';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 
 @Injectable({
@@ -19,7 +20,8 @@ export class AvatarService {
     private firestore: Firestore,
     private storage: Storage,
     private authService: AuthService,
-    private firestoreService: FirestoreService
+    private firestoreService: FirestoreService,
+    private storage2: AngularFireStorage
   ) {}
 
   getUserProfile() {
@@ -28,11 +30,7 @@ export class AvatarService {
     return docData(userDocRef);
   }
 
-  getPet() {
-    const user = this.auth.currentUser;
-    const userDocRef = doc(this.firestore, `reportes/${user.uid}`);
-    return docData(userDocRef);
-  }
+ 
 
   async uploadImage(cameraFile: Photo) {
     const user = this.auth.currentUser;
@@ -54,11 +52,11 @@ export class AvatarService {
     }
   }
 
-
-/*   uploadImage2(file: any, path: string, nombre: string): Promise<string> {
+/* 
+  uploadImageReporte(file: any, path: string, nombre: string): Promise<string> {
     return new Promise(  resolve => {
-        const filePath = path + '/' + nombre;
-        const ref = this.storage.  ref(filePath);
+        const filePath = path + '/' + nombre + '.png';
+        const ref = this.storage2.ref(filePath);
         const task = ref.put(file);
         task.snapshotChanges().pipe(
           finalize(  () => {
@@ -71,12 +69,43 @@ export class AvatarService {
        )
       .subscribe();
     });
-}
- */
+} */
+
       
+//FUNCION QUE SUBE FOTO DE MASCOTA
+async uploadPhoto(cameraFile: Photo, path : string, nombre: string) {
+  const filePath = path + '/' + nombre + '/imagen.png';
+  /* const path = `/imagen.png`; */
+  const user = this.auth.currentUser;
+  const storageRef = ref(this.storage, filePath);
+
+  try {
+    await uploadString(storageRef, cameraFile.base64String, 'base64');
+
+    const url = await getDownloadURL(storageRef);
+    const uid = this.auth.currentUser.uid
+    return url
+  } catch (e) {
+    return null;
+  }
+}
+
+getPetImage(token){
+  const userDocRef = doc(this.firestore, `reportes/${token}`);
+  return docData(userDocRef);
+
+}
+
+
+getPet() {
+  const user = this.auth.currentUser;
+  const userDocRef = doc(this.firestore, `reportes/${user.uid}`);
+  return docData(userDocRef);
+}
+
 
 //FUNCION QUE SUBE FOTO DE MASCOTA
-  async uploadPhoto(cameraFile: Photo) {
+  /* async uploadPhoto(cameraFile: Photo) {
     const user = this.auth.currentUser;
     const randomid = this.firestoreService.createRandomID();
     const userContador = user + '1';
@@ -95,5 +124,5 @@ export class AvatarService {
     } catch (e) {
       return null;
     }
-  }
+  } */
 }
