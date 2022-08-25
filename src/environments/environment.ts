bb7ec5f203ae700f13087f7d697d909da6e3b6e6
Marcel0M <marcel.moralesc@duocuc.cl>
2022-08-24 20:03:20 -0400

    hidek

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,17 +4,17 @@
 
 export const environment = {
   firebase: {
-    projectId: 'hogartempo',
-    appId: '1:356056792213:web:4273a9277be195b832af9c',
-    storageBucket: 'hogartempo.appspot.com',
+    projectId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    appId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    storageBucket: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
     locationId: 'us-central',
-    apiKey: 'AIzaSyDXfdEbK4FsyISFKmaxvLZSHQ4j79B5ywU',
-    authDomain: 'hogartempo.firebaseapp.com',
-    messagingSenderId: '356056792213',
-    measurementId: 'G-N5T2W4M085',
+    apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    authDomain: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    messagingSenderId: '3xxxxxxxxxxxxxxxxxxxxxx3',
+    measurementId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
   },
   production: false,
-  apiKeyMaps: 'AIzaSyAg-oZFveCX-c12Ymc95jstr5VULGvtEpo',
+  apiKeyMaps: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
 };
 
 /*
